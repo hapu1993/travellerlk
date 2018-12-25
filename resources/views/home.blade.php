6a703e45384c64efe9ba1c27f3f51cdddc6cd7c8
amila <hapuarachchi.93@gmail.com>
2018-12-25 14:33:51 +0530

    Home Page Slider Developments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,108 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="col-md-12">
+
+	<div class="row">
+
+		<div class="col-md-6">
+
+			<div class="row">
+
+				<img src="{{URL('assets/images/home6.jpeg')}}" alt="" class="homeimage">
+
+			</div>
+		</div>
+
+		<div class="col-md-6">
+			<div class="row">
+				<!-- Start WOWSlider.com BODY section -->
+				<div id="wowslider-container1">
+					<div class="ws_images"><ul>
+						<li><a href="http://wowslider.net"><img src="{{URL('assets/data1/images/5.jpg')}}" alt="javascript slider" title="fixedw_large_4x" id="wows1_0"/></a></li>
+						<li><img src="{{URL('assets/data1/images/6.jpg')}}" alt="Hiking-trails-around-Ella-Sri-Lanka-Idalgashima-3" title="Hiking-trails-around-Ella-Sri-Lanka-Idalgashima-3" id="wows1_1"/></li>
+						<li><a href="http://wowslider.net"><img src="{{URL('assets/data1/images/3.jpg')}}" alt="javascript slider" title="fixedw_large_4x" id="wows1_0"/></a></li>
+						<li><img src="{{URL('assets/data1/images/2.jpg')}}" alt="Hiking-trails-around-Ella-Sri-Lanka-Idalgashima-3" title="Hiking-trails-around-Ella-Sri-Lanka-Idalgashima-3" id="wows1_1"/></li>
+						<li><a href="http://wowslider.net"><img src="{{URL('assets/data1/images/4.jpg')}}" alt="javascript slider" title="fixedw_large_4x" id="wows1_0"/></a></li>
+						<li><a href="http://wowslider.net"><img src="{{URL('assets/data1/images/7.jpg')}}" alt="javascript slider" title="fixedw_large_4x" id="wows1_0"/></a></li>
+
+					</ul></div>
+					<div class="ws_bullets"><div>
+						<a href="#" title="fixedw_large_4x"><span><img src="{{URL('assets/data1/tooltips/fixedw_large_4x.jpg')}}" alt="fixedw_large_4x"/>1</span></a>
+						<a href="#" title="Hiking-trails-around-Ella-Sri-Lanka-Idalgashima-3"><span><img src="{{URL('assets/data1/tooltips/hikingtrailsaroundellasrilankaidalgashima3.jpg')}}" alt="Hiking-trails-around-Ella-Sri-Lanka-Idalgashima-3"/>2</span></a>
+					</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">slider jquery</a> by WOWSlider.com v8.8</div>
+					<div class="ws_shadow"></div>
+				</div>
+				<script type="text/javascript" src="{{URL('assets/engine1/wowslider.js')}}"></script>
+				<script type="text/javascript" src="{{URL('assets/engine1/script.js')}}"></script>
+				<!-- End WOWSlider.com BODY section -->
+
+
+			</div>
+			<div class="col-md-12">
+				<div class="row">
+
+
+
+					<img src="{{url('assets/images/home4.jpeg')}}" alt="" class="banner1">
+
+				</div>
+			</div>
+		</div>
+
+	</div>
 </div>
+
+<div class="container">
+  <h2>Our  Partners/ Our Clients</h2>
+   <section class="customer-logos slider">
+      <div class="slide"><img src="{{URL('assets/images/logoslider/logo1.png')}}"></div>
+      <div class="slide"><img src="{{URL('assets/images/logoslider/logo2.png')}}"></div>
+      <div class="slide"><img src="{{URL('assets/images/logoslider/logo3.png')}}"></div>
+      <div class="slide"><img src="{{URL('assets/images/logoslider/logo4.png')}}"></div>
+      <div class="slide"><img src="{{URL('assets/images/logoslider/logo5.jpg')}}"></div>
+      <div class="slide"><img src="{{URL('assets/images/logoslider/logo6.png')}}"></div>
+      <div class="slide"><img src="{{URL('assets/images/logoslider/logo7.png')}}"></div>
+      <div class="slide"><img src="{{URL('assets/images/logoslider/logo8.png')}}"></div>
+      <div class="slide"><img src="{{URL('assets/images/logoslider/logo9.png')}}"></div>
+			<div class="slide"><img src="{{URL('assets/images/logoslider/logo10.png')}}"></div>
+
+   </section>
+
+
+</div>
+
+
+@endsection
+
+@section('scripts')
+<script type="text/javascript" src="{{URL('assets/engine1/wowslider.js')}}"></script>
+<script type="text/javascript" src="{{URL('assets/engine1/script.js')}}"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+		$('.customer-logos').slick({
+				slidesToShow: 6,
+				slidesToScroll: 1,
+				autoplay: true,
+				autoplaySpeed: 1500,
+				arrows: false,
+				dots: false,
+				pauseOnHover: false,
+				responsive: [{
+						breakpoint: 768,
+						settings: {
+								slidesToShow: 4
+						}
+				}, {
+						breakpoint: 520,
+						settings: {
+								slidesToShow: 3
+						}
+				}]
+		});
+});
+</script>
+
 @endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{URL('assets/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{URL('assets/css/styles.css')}}">
+    <!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="{{URL('assets/engine1/style.css')}}" />
+<script type="text/javascript" src="{{URL('assets/engine1/jquery.js')}}"></script>
+
+<!-- End WOWSlider.com HEAD section -->
+<style media="screen">
+/* Footer */
+@import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+section {
+  padding: 60px 0;
+}
+
+section .section-title {
+  text-align: center;
+  color: #007b5e;
+  margin-bottom: 50px;
+  text-transform: uppercase;
+}
+#footer {
+  background: #007b5e !important;
+}
+#footer h5{
+padding-left: 10px;
+  border-left: 3px solid #eeeeee;
+  padding-bottom: 6px;
+  margin-bottom: 20px;
+  color:#ffffff;
+}
+#footer a {
+  color: #ffffff;
+  text-decoration: none !important;
+  background-color: transparent;
+  -webkit-text-decoration-skip: objects;
+}
+#footer ul.social li{
+padding: 3px 0;
+}
+#footer ul.social li a i {
+  margin-right: 5px;
+font-size:25px;
+-webkit-transition: .5s all ease;
+-moz-transition: .5s all ease;
+transition: .5s all ease;
+}
+#footer ul.social li:hover a i {
+font-size:30px;
+margin-top:-10px;
+}
+#footer ul.social li a,
+#footer ul.quick-links li a{
+color:#ffffff;
+}
+#footer ul.social li a:hover{
+color:#eeeeee;
+}
+#footer ul.quick-links li{
+padding: 3px 0;
+-webkit-transition: .5s all ease;
+-moz-transition: .5s all ease;
+transition: .5s all ease;
+}
+#footer ul.quick-links li:hover{
+padding: 3px 0;
+margin-left:5px;
+font-weight:700;
+}
+#footer ul.quick-links li a i{
+margin-right: 5px;
+}
+#footer ul.quick-links li:hover a i {
+  font-weight: 700;
+}
+
+@media (max-width:767px){
+#footer h5 {
+  padding-left: 0;
+  border-left: transparent;
+  padding-bottom: 0px;
+  margin-bottom: 10px;
+}
+}
+
+</style>
+    @yield('style')
+  </head>
+  <body>
+    @include('includes.header')
+    @yield('content')
+
+
+
+    @include('includes.footer')
+    <script src="{{URL('assets/js/jquery-3.3.1.js')}}"></script>
+    <script src="{{URL('assets/js/bootstrap.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+  </body>
+
+  @yield('scripts')
+</html>
